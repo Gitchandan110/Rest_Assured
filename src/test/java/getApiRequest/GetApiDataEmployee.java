d1922278412a460e1018208a2bf566bc6cffeaff
chandan kumar <chandan.kumar@netsutra.com>
2021-05-18 00:24:25 +0530

    cu

--- src/test/java/getApiRequest/GetApiDataEmployee.java
+++ src/test/java/getApiRequest/GetApiDataEmployee.java
@@ -1,12 +1,9 @@
 package getApiRequest;
 
 import static org.testng.Assert.assertEquals;
-
 import java.util.concurrent.TimeUnit;
-
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 
@@ -20,9 +17,18 @@ public class GetApiDataEmployee {
 		
 		Response responce=RestAssured.get("http://dummy.restapiexample.com/api/v1/employee/1");
 		System.out.println("Responce Time is: "+ responce.getTimeIn(TimeUnit.SECONDS) );
+		
 		int statuscode=responce.getStatusCode();
 		
-		System.out.println("Status Code is: "+ statuscode );
+		System.out.println("Status Code is: " + statuscode );
+		
+		System.out.println("responce.asString() is : " +responce.asString());
+		
+		System.out.println("responce.getStatusLine() is : " +responce.getStatusLine());
+		
+		System.out.println("responce.getHeader_content-type is : " + responce.getHeader("content-type"));
+		
+		System.out.println("responce.getTime() is : " + responce.getTime());
 		
 		Assert.assertEquals(statuscode, 200);
 		
